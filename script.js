5ec874e70eb69d29f9c6553b539eac7ec5f1c604
Sadman Mahmud <sadmanmahmud001@gnail.com>
2025-05-01 23:37:06 +0600

    facebook is done and email js is added

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 "use strict";
 
+const contactForm = document.getElementById("contact-form");
+const contactName = document.getElementById("contact-name");
+const contactEmail = document.getElementById("contact-email");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  if (contactName.value === "" || contactEmail.value === "") {
+    console.log("hi");
+  } else {
+    emailjs
+      .sendForm(
+        "service_hxf6e5a",
+        "template_pcr9kjc",
+        "#contact-form",
+        "8dabSZqO4hCxq4lAU"
+      )
+      .then(
+        () => {
+          console.log("hello");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+  }
+};
