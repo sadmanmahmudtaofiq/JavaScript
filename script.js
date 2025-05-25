361b6c888b9f633292f204b10ca17c49a088a013
Sadman Mahmud <sadmanmahmud001@gnail.com>
2025-05-25 21:25:41 +0600

    commiting for push code to gitlab, first push on gitlab 25-5-2025

--- script.js
+++ script.js
@@ -1,29 +1,110 @@
 "use strict";
 
-const contactForm = document.getElementById("contact-form");
-const contactName = document.getElementById("contact-name");
-const contactEmail = document.getElementById("contact-email");
+const textToSpeach = () => {
+  const input = document.querySelector(".input");
+  const btn = document.querySelector(".btn");
 
-const sendEmail = (e) => {
-  e.preventDefault();
+  const textToSpeach = () => {
+    let text = input.value;
+    const voice = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(voice);
+  };
 
-  if (contactName.value === "" || contactEmail.value === "") {
-    console.log("hi");
-  } else {
-    emailjs
-      .sendForm(
-        "service_hxf6e5a",
-        "template_pcr9kjc",
-        "#contact-form",
-        "8dabSZqO4hCxq4lAU"
-      )
-      .then(
-        () => {
-          console.log("hello");
-        },
-        (error) => {
-          console.log("FAILED...", error);
-        }
-      );
-  }
+  btn.addEventListener("click", textToSpeach);
 };
+
+// textToSpeach();
+
+const stacks = [
+  {
+    name: "MERN Stack",
+    components: "MongoDB, Express, React, Node",
+    language: "JavaScript",
+    popular: true,
+    cost: 3200,
+  },
+  {
+    name: "Django",
+    components: "Python, Django, PostgreSQL",
+    language: "Python",
+    popular: true,
+    cost: 3800,
+  },
+  {
+    name: "Ruby on Rails",
+    components: "Ruby, Rails, SQLite",
+    language: "Ruby",
+    popular: true,
+    cost: 2900,
+  },
+  {
+    name: "Spring Boot",
+    components: "Java, Spring, Hibernate, MySQL",
+    language: "Java",
+    popular: false,
+    cost: 4200,
+  },
+  {
+    name: "Laravel",
+    components: "Javascript, Vue, PHP, Laravel, MySQL",
+    language: "PHP",
+    popular: false,
+    cost: 4000,
+  },
+  {
+    name: "C System Stack",
+    components: "C, Linux, Make, GCC",
+    language: "C",
+    popular: true,
+    cost: 2500,
+  },
+  {
+    name: "Qt Framework",
+    components: "C++, Qt, CMake, GCC",
+    language: "C++",
+    popular: false,
+    cost: 4500,
+  },
+];
+
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  const inputValue = input.value.toLowerCase().trim();
+
+  const getStack = stacks.filter((sk) => sk.language.toLowerCase() === inputValue);
+  console.log(getStack, getStack.length);
+
+  let html = "";
+
+  if (getStack.length > 0) {
+    getStack.forEach((i) => {
+      const { name, components, language, popular, cost } = i;
+    
+
+    const popularClass = popular ? "Popular" : "Not popular";
+
+    html += `
+    
+      <h1>${language} Technology Stacks</h1>
+            
+      <div class="stack-card">
+          <h2 class="stack-name">${name}
+              <span class="not-popular">${popularClass}</span>
+          </h2>
+          <p><strong>Components:</strong> ${components}</p>
+          <p><strong>Language:</strong> ${language}</p>
+          <p><strong>Price:</strong> $<span class="price">${cost}</span></p>
+      </div>
+    
+    `;
+    });
+  } 
+  
+  else {
+    html = `<p class="no-results">No stacks found for language "${inputValue}"</p>`;
+  }
+
+  document.querySelector("section").innerHTML = html;
+});
