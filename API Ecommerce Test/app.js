4bd0557cacbe32df81d446b0f6398497ae7ccbd7
Sadman Mahmud <sadmanmahmud001@gnail.com>
2025-10-10 00:36:50 +0600

    API Ecommerce Test Is Done

---
+++ API Ecommerce Test/app.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const URL = `https://dummyjson.com/products?limit=500`;
+
+const getData = async () => {
+  let response = await fetch(URL);
+  let responseData = await response.json();
+  let data = responseData.products;
+  createElement(data);
+  console.log(data.length);
+};
+
+function ratingFunc() {
+  let rateNum = [4, 45, 5];
+  let randomNum = rateNum[Math.floor(Math.random() * rateNum.length)];
+  return randomNum;
+}
+
+function createElement(data) {
+  let code = ``;
+  data.forEach((item) => {
+    let { title, category, description, price, minimumOrderQuantity } = item;
+    let html = `
+    <section>
+        <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+            <img
+                src="${item.images[0]}"
+                alt="${category}"
+            />
+            </div>
+
+            <div class="flip-card-back">
+            <img
+                src="${item.images[0]}"
+                alt="${category}"
+            />
+            <h2>${category}</h2>
+            <p class="hoverTitle">
+                ${title}
+            </p>
+            <p class="description">
+                ${description}
+            </p>
+            </div>
+        </div>
+        </div>
+
+        <div class="info">
+        <p class="title" title="${title}">
+            ${title}
+        </p>
+        <div class="rating">
+            <img
+            class="ratingImg"
+            src="https://sadmanmahmudtaofiq.github.io/Amazon/Photos/rating-${ratingFunc()}.png"
+            alt=""
+            />
+            <p class="ratingText" title="${minimumOrderQuantity}">${minimumOrderQuantity}</p>
+        </div>
+        <p class="price" title="${price}">$${price}</p>
+        </div>
+        <div class="btn">
+        <button title="Add to Cart">Add to Cart</button>
+        </div>
+    </section>
+    `;
+    code += html;
+  });
+  document.querySelector(".container").innerHTML = code;
+}
+
+getData();
+
